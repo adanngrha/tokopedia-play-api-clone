ed8c7532e19e22aec4a7e5d658b9eeaaf0680b86
adanngrha <contact.ardana@gmail.com>
2023-08-14 22:45:44 +0700

    changed to all users can comment

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,7 @@ const { getAll, create, onDelete } = require('../controllers/comments');
 const {authentication} = require("../middlewares/auth");
 
 router.get('/:videoId/comments', getAll);
-router.post('/:videoId/comments', authentication, create);
+router.post('/:videoId/comments', create);
 router.delete('/:videoId/comments/:commentId', authentication, onDelete);
 
 module.exports = router;
